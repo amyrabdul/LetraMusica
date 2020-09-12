10f0facc4deb5870a3fecf8e9c50c533d0f8b24d
amyrabdul <amyrabdul@hotmail.com>
2020-09-11 22:29:38 -0300

    exercicio musica

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LetraMusica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Voce conhece Chester Bennington?");
+            Console.ReadKey ();
+            Console.WriteLine("Ele era o Vocalista da Banda Linkin Park");
+            Console.ReadKey ();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Aqui vai uma musica dele que se chama NUMB");
+            Console.ReadKey ();
+            Console.ResetColor ();
+            Console.WriteLine("I'm tired of being what you want me to be\n Feeling so faithless, lost under the surface\n Don't know what you're expecting of me\n Put under the pressure of walking in your shoes\n Caught in the undertow, just caught in the undertow\n Every step that I take is another mistake to you\n Caught in the undertow, just caught in the undertow\n" );
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine ("Ive become so numb, I cant feel you there\n Become so tired, so much more aware\n Im becoming this, all I want to do\n Is be more like me and be less like you");
+            Console.ResetColor ();
+            Console.WriteLine("Can't you see that you're smothering me\n Holding too tightly, afraid to lose control\n 'Cause everything that you thought I would be\n Has fallen apart right in front of you\n (Caught in the undertow, just caught in the undertow)\n Every step that I take is another mistake to you\n (Caught in the undertow, just caught in the undertow)\n And every second I waste is more than I can take");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine ("Ive become so numb, I cant feel you there\n Become so tired, so much more aware\n Im becoming this, all I want to do\n Is be more like me and be less like you");
+            Console.ResetColor ();
+            Console.WriteLine ("And I know I may end up failing too\n But I know you were just like me with someone disappointed in you");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine ("Ive become so numb, I cant feel you there\n Become so tired, so much more aware\n Im becoming this, all I want to do\n Is be more like me and be less like you");
+            Console.ResetColor ();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine ("Ive become so numb, I cant feel you there\n Become so tired, so much more aware\n Im becoming this, all I want to do\n Is be more like me and be less like you");
+            Console.ReadKey ();
+            Console.ResetColor ();
+            Console.WriteLine("Muito boa!");
+
+
+
+
+            Console.ReadKey ();
+
+        }
+    }
+}
